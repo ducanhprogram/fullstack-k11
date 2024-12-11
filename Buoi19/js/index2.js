9939439ef78eefa9bfa7d64d36fffb10f46b9b80
Duc Anh <leducanhled@gmail.com>
2024-12-12 00:10:59 +0700

    Sửa lại bài tập Day19

--- Buoi19/js/index2.js
+++ Buoi19/js/index2.js
@@ -1,7 +1,7 @@
 const generateMultiplicationTable = () => {
     let result = "";
     for (let i = 1; i <= 10; i++) {
-        result += `<h1> Bảng cửu chương số ${i} </h1><br>`;
+        result += `<h2> Bảng cửu chương số ${i} </h2><br>`;
         for (let j = 1; j <= 10; j++) {
             result += `${i} * ${j} = ${i * j} <br>`;
         }
